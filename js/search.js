26b44ba28fa0b273f9685f61c9555b5ed23f0d5a
Soleil Walker <149134310+sunnysoleil@users.noreply.github.com>
2024-08-19 14:31:51 -0500

    Refactor search function in search.js

--- js/search.js
+++ js/search.js
@@ -2,15 +2,14 @@ let inputField = document.querySelector("input");
 inputField.addEventListener("keyup", searchFunction);
 
 function searchFunction() {
-    let currentInput = inputField.value.toLowerCase;
+    let currentInput = inputField.value.toLowerCase();
     let galleryItem = document.querySelectorAll("a");
-    for (i = 0; i < galleryItem.length; i++) {
-        let caption = galleryItem[i].getAttribute('data-caption');
-        if (caption.toLowerCase().includes(currentInput)) {
-            caption[i].style.display = "";
+    galleryItem.forEach(photo => {
+        let photoCaption = photo.getAttribute('data-caption').toLowerCase();
+        if (!photoCaption.includes(currentInput)) {
+            photo.style.display = "none";
         } else {
-            caption[i].style.display = "none";
+            photo.style.display = "";
         }
-    }
+    });
 }
-
